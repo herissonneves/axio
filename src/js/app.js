3e2f1c001cbf9d25b22333832be40853d6368dbe
Herisson Nobre <herisson.carvalho96@gmail.com>
2025-11-26 10:48:50 -0300

    feat(js): implement filtering logic and update render flow

--- src/js/app.js
+++ src/js/app.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.getElementById('todo-input');
     const list = document.getElementById('todo-list');
 
+    let filter = 'all'; // 'all' | 'active' | 'completed'
+
     // Try to load tasks from localStorage
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    //
+    // Create list items for each task
     tasks.forEach(task => {
         createTaskElement(task);
     });
@@ -30,15 +32,28 @@ document.addEventListener('DOMContentLoaded', () => {
         input.focus();
     });
 
+    document.getElementById('filter-all').addEventListener('click', () => {
+        filter = 'all';
+        renderTasks(tasks);
+    })
+
+    document.getElementById('filter-active').getElementById('click', () => {
+        filter = 'active';
+        renderTasks(tasks);
+    })
+
+    document.getElementById('filter-completed').addEventListener('click', () => {
+        filter = 'completed';
+        renderTasks(tasks);
+    })
+
     function createTaskElement(task) {
         const li = document.createElement('li');
         li.setAttribute('data-id', task.id);
 
         const span = document.createElement('span');
         span.textContent = task.text;
-        if(span.completed) {
-            li.classList.add('completed');
-        }
+        if (task.completed) li.classList.add('completed');
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = '✖';
@@ -46,16 +61,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         // Mark the task as complete on the click of text
         span.addEventListener('click', () => {
-            span.classList.toggle('completed');
             task.completed = !task.completed;
             saveTasks();
+            renderTasks(tasks);
         });
 
         // Remove the task on the click of the remove button
         removeBtn.addEventListener('click', () => {
-            list.removeChild(li);
             tasks = tasks.filter(t => t.id !== task.id);
             saveTasks();
+            renderTasks(tasks);
         });
 
         li.appendChild(span);
@@ -65,5 +80,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function saveTasks() {
         localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    function renderTasks(tasks) {
+        list.innerHTML = '';
+        tasks.filter(task =>{
+            if (filter === 'all') return true;
+            if (filter === 'active') return task.completed === false;
+            if (filter === 'completed') return task.completed === true;
+        }).forEach(task => createTaskElement(task));
     }
 })
